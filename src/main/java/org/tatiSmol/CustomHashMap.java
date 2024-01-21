cf2068510fb61248568dee103519109a5077d806
tatiSmol <judeclapton@icloud.com>
2024-01-21 23:32:24 +0300

    Added three constructors for CustomHashMap class with descriptions

--- src/main/java/org/tatiSmol/CustomHashMap.java
+++ src/main/java/org/tatiSmol/CustomHashMap.java
@@ -5,7 +5,49 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * CustomHashMap class implements Map and Iterable interfaces.
+ * This is a custom implementation of HashMap class.
+ *
+ * @param <K> the type of keys maintained by this map
+ * @param <V> the type of mapped values
+ */
 public class CustomHashMap<K, V> implements Map<K, V>, Iterable<Map.Entry<K, V>> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] table;
+    private int size = 0;
+
+    /**
+     * Constructs an empty CustomHashMap with the default initial capacity (16).
+     */
+    public CustomHashMap() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * Constructs an empty CustomHashMap with the custom initial capacity.
+     *
+     * @param initialCapacity the initial capacity of the hash map. Must be non-negative.
+     * @throws IllegalArgumentException if capacity less than 0.
+     */
+    public CustomHashMap(int initialCapacity) {
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("Capacity can't be negative");
+        }
+        table = new Node[initialCapacity];
+    }
+
+    /**
+     * Constructs CustomHashMap with the default initial capacity (16) and
+     * adds all key-value pairs from the specified map.
+     *
+     * @param m map containing the key-value pairs that will be added to the new hash map.
+     */
+    public CustomHashMap(Map<? extends K, ? extends V> m) {
+        this();
+        putAll(m);
+    }
 
     @Override
     public int size() {
